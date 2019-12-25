667f235178dc12b665c03c4a430e91e9965fe844
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2019-12-25 14:47:39 +0100

    first commit

---
+++ Asp.netCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Employee
+    {   [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName ="nvarchar(250)")]
+        [DisplayName("Name")]
+        [Required(ErrorMessage ="This field is required")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+      
+        public string Position { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+
+        public string officeLocation { get; set; }
+
+    }
+}
